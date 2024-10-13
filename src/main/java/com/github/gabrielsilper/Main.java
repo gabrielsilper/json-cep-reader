913a43a7b626ebd8d65bd80526b6b2c0508c1002
gabrielsilper <gabrielsilper.main@gmail.com>
2024-10-13 15:27:26 -0400

    Chore: Pequenos ajustes

--- src/main/java/com/github/gabrielsilper/Main.java
+++ src/main/java/com/github/gabrielsilper/Main.java
@@ -20,26 +20,35 @@ public class Main {
         File dir = new File("json-ceps");
         File[] jsons = dir.listFiles();
 
-        if (Objects.nonNull(jsons)) {
-            // Testes para melhorar o desempenho, com esse sleep para dar folga as operações dd banco, foi 3x mais rápido.
-            System.out.println(jsons.length);
-            for (int i = 0; i < 10000; i++) {
-                CEP cep = cepJsonReader.read(jsons[i]);
-                cepDao.addCEP(con, cep);
+        try {
+            con.setAutoCommit(false);
+            if (Objects.nonNull(jsons)) {
+                // Testes para melhorar o desempenho, com esse sleep para dar folga as operações dd banco, foi 3x mais rápido.
+                System.out.println(jsons.length);
+                for (int i = 0; i < 10000; i++) {
+                    if (jsons[i].getName().endsWith(".json")) {
+                        CEP cep = cepJsonReader.read(jsons[i]);
+                        cepDao.addCEP(con, cep);
+                    }
+                }
+                Thread.sleep(2000);
+                for (int i = 10000; i < 20000; i++) {
+                    CEP cep = cepJsonReader.read(jsons[i]);
+                    cepDao.addCEP(con, cep);
+                }
+                Thread.sleep(2000);
+                for (int i = 20000; i < 30000; i++) {
+                    CEP cep = cepJsonReader.read(jsons[i]);
+                    cepDao.addCEP(con, cep);
+                }
+                System.out.println("CEPs adicionados");
             }
-            Thread.sleep(2000);
-            for (int i = 10000; i < 20000; i++) {
-                CEP cep = cepJsonReader.read(jsons[i]);
-                cepDao.addCEP(con, cep);
-            }
-            Thread.sleep(2000);
-            for (int i = 20000; i < 30000; i++) {
-                CEP cep = cepJsonReader.read(jsons[i]);
-                cepDao.addCEP(con, cep);
-            }
-            System.out.println("CEPs adicionados");
+            con.commit();
+        } finally {
+            con.rollback();
+            con.setAutoCommit(true);
+            con.close();
         }
-        con.close();
 
         long endTime = System.nanoTime();
         double durationInSeconds = (endTime - startTime) / 1_000_000_000.0; // Converte para segundos

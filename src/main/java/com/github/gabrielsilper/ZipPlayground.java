e0f9c8c0e39e44162686d469976d3e6bcd712d2c
gabrielsilper <gabrielsilper.main@gmail.com>
2024-10-13 16:22:51 -0400

    Feat: Função pra descompactar um arquivo zip

--- src/main/java/com/github/gabrielsilper/ZipPlayground.java
+++ src/main/java/com/github/gabrielsilper/ZipPlayground.java
@@ -1,5 +1,6 @@
 package com.github.gabrielsilper;
 
+import org.zeroturnaround.zip.NameMapper;
 import org.zeroturnaround.zip.ZipUtil;
 
 import java.io.File;
@@ -8,7 +9,12 @@ public class ZipPlayground {
     public static void main(String[] args) {
         boolean exists = ZipUtil.containsEntry(new File("json-ceps.zip"), "json-ceps");
         if(exists){
-            ZipUtil.explode(new File("json-ceps.zip"));
+            final String prefix = "json-ceps/";
+            ZipUtil.unpack(new File("json-ceps.zip"), new File("json-ceps"), new NameMapper() {
+                public String map(String name) {
+                    return name.startsWith(prefix) ? name.substring(prefix.length()) : name;
+                }
+            });
         }
     }
 }
